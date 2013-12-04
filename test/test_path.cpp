85eb1a4677666843da5aec92bd231adde489c3ed
mythagel <mythagel@gmail.com>
2013-12-05 10:24:49 +1100

    test update

--- test/test_path.cpp
+++ test/test_path.cpp
@@ -1,127 +1,96 @@
 #include "pathparser.h"
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <stdexcept>
 
-struct pathwriter : svg::path::parser
+struct normalise_path : svg::path::parser
 {
-    float startx;
-    float starty;
-    float cpx;
-    float cpy;
+    std::stringstream s;
     
 	void move_to(bool abs, float x, float y) override
 	{
-	    if(abs)
-    	    std::cerr << "move_to(" << x << ", " << y << ")\n";
-	    else
-	        std::cerr << "rel_move_to(" << x << ", " << y << ")\n";
-        startx = cpx = x;
-        starty = cpy = y;
+	    s << (abs?"M" : "m") << " " << x << " " << y << " ";
 	}
 
 	void line_to(bool abs, float x, float y) override
 	{
-	    if(abs)
-    	    std::cerr << "line_to(" << x << ", " << y << ")\n";
-	    else
-	        std::cerr << "rel_line_to(" << x << ", " << y << ")\n";
-        cpx = x;
-        cpy = y;
+    	s << (abs?"L" : "l") << " " << x << " " << y << " ";
 	}
 	void horizontal_line_to(bool abs, float x) override
 	{
-	    if(abs)
-    	    std::cerr << "horizontal_line_to(" << x << ")\n";
-	    else
-	        std::cerr << "rel_horizontal_line_to(" << x << ")\n";
-        cpx = x;
+	    s << (abs?"H" : "h") << " " << x << " ";
 	}
 	void vertical_line_to(bool abs, float y) override
 	{
-	    if(abs)
-    	    std::cerr << "vertical_line_to(" << y << ")\n";
-	    else
-	        std::cerr << "rel_vertical_line_to(" << y << ")\n";
-        cpy = y;
+    	s << (abs?"V" : "v") << " " << y << " ";
 	}
 
 	void curve_to(bool abs, float x1, float y1, float x2, float y2, float x, float y) override
 	{
-	    if(abs)
-    	    std::cerr << "curve_to(" << x1 << ", " << y1 << ", " << x2 << ", " << y2 << ", " << x << ", " << y << ")\n";
-	    else
-	        std::cerr << "rel_curve_to(" << x1 << ", " << y1 << ", " << x2 << ", " << y2 << ", " << x << ", " << y << ")\n";
-        cpx = x;
-        cpy = y;
+    	s << (abs?"C" : "c") << " " << x1 << " " << y1 << " " << x2 << " " << y2 << " " << x << " " << y << " ";
 	}
 	void smooth_curve_to(bool abs, float x2, float y2, float x, float y) override
 	{
-	    if(abs)
-    	    std::cerr << "smooth_curve_to(" << x2 << ", " << y2 << ", " << x << ", " << y << ")\n";
-	    else
-	        std::cerr << "rel_smooth_curve_to(" << x2 << ", " << y2 << ", " << x << ", " << y << ")\n";
-        cpx = x;
-        cpy = y;
+    	s << (abs?"S" : "s") << " " << x2 << " " << y2 << " " << x << " " << y << " ";
 	}
 
 	void bezier_curve_to(bool abs, float x1, float y1, float x, float y) override
 	{
-	    if(abs)
-    	    std::cerr << "bezier_curve_to(" << x1 << ", " << y1 << ", " << x << ", " << y << ")\n";
-	    else
-	        std::cerr << "rel_bezier_curve_to(" << x1 << ", " << y1 << ", " << x << ", " << y << ")\n";
-        cpx = x;
-        cpy = y;
+    	s << (abs?"Q" : "q") << " " << x1 << " " << y1 << " " << x << " " << y << " ";
 	}
 	void smooth_bezier_curve_to(bool abs, float x, float y) override
 	{
-	    if(abs)
-    	    std::cerr << "smooth_bezier_curve_to(" << x << ", " << y << ")\n";
-	    else
-	        std::cerr << "rel_smooth_bezier_curve_to(" << x << ", " << y << ")\n";
-        cpx = x;
-        cpy = y;
+    	s << (abs?"T" : "t") << " " << x << " " << y << " ";
 	}
 
 	void close_path() override
 	{
-	    std::cerr << "close_path(" << startx << ", " << starty << ")\n";
-        cpx = startx;
-        cpy = starty;
+	    s << "Z" << " ";
 	}
 	void eof() override
 	{
 	}
 };
 
-void do_path(const std::string& path)
+void check_path(const std::string& path, const std::string& expected)
 {
-    pathwriter parser;
+    normalise_path parser;
     auto c = path.c_str();
     auto end = c + path.size();
     
     parser.parse(c, end);
+    
+    if(parser.s.str() != expected)
+        throw std::logic_error(path + " Not parsed to '" + expected + "' actual: '" + parser.s.str() + "'");
+    std::cout << "'" << path << "' -> '" << parser.s.str() << "'\n";
+}
+
+inline bool throw_if(bool cond, const std::string& what)
+{
+    if(cond) throw std::logic_error(what);
+    return cond;
 }
 
 int main()
 {
-    do_path("M 100 100 L 200 200");
-    do_path("M100 100L200 200");
-    do_path("M100 100 200 200");
+    check_path("M 100 100 L 200 200", "M 100 100 L 200 200 ");
+    check_path("M100 100L200 200", "M 100 100 L 200 200 ");
+    check_path("M100 100 200 200", "M 100 100 L 200 200 ");
     
-    do_path("M 100 200 L 200 100 L -100 -200");
-    do_path("M 100 200 L 200 100 -100 -200");
-    do_path("M 100-200");
-    do_path("M 0.6.5");
+    check_path("M 100 200 L 200 100 L -100 -200", "M 100 200 L 200 100 L -100 -200 ");
+    check_path("M 100 200 L 200 100 -100 -200", "M 100 200 L 200 100 L -100 -200 ");
+    check_path("M 100-200", "M 100 -200 ");
+    check_path("M 0.6.5", "M 0.6 0.5 ");
 
-    do_path("M100,100L200,200");
-    do_path("M100,100L200,200");
-    do_path("M100,100,200,200");
+    check_path("M100,100L200,200", "M 100 100 L 200 200 ");
+    check_path("M100,100L200,200", "M 100 100 L 200 200 ");
+    check_path("M100,100,200,200", "M 100 100 L 200 200 ");
     
-    do_path("M100,200L200,100L-100,-200");
-    do_path("M100,200L200,100,-100,-200");
-    do_path("M100,-200");
-    do_path("M0.6,.5");
+    check_path("M100,200L200,100L-100,-200", "M 100 200 L 200 100 L -100 -200 ");
+    check_path("M100,200L200,100,-100,-200", "M 100 200 L 200 100 L -100 -200 ");
+    check_path("M100,-200", "M 100 -200 ");
+    check_path("M0.6,.5", "M 0.6 0.5 ");
 
     return 0;
 }
