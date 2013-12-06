94caeee270b5015dadeffa0ade8b1e9b243048f7
mythagel <mythagel@gmail.com>
2013-12-06 15:20:09 +1100

    More playing

--- test/test_document.cpp
+++ test/test_document.cpp
@@ -8,7 +8,10 @@ int main()
     auto l2 = line;
     
     std::cout << *l2.x1 << "\n";
+    std::cout << *l2.x2 << "\n";
     std::cout << l2.get("x1").get() << "\n";
+    std::cout << l2.get("x2").get() << "\n";
+//    std::cout << l2.get("x3").get() << "\n";
     return 0;
 }
 
